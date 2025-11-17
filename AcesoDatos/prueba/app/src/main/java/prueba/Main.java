b69067c07d23866fda7abf92297c872e15ff4d09
diegoteis1222 <diegoteis122@gmail.com>
2025-11-17 09:30:47 +0100

    ejercicio Conexion con y sin

---
+++ AcesoDatos/prueba/app/src/main/java/prueba/Main.java
@@ -0,0 +1,39 @@
+package prueba;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String conn = "jdbc:mysql://localhost:3306/Northwind";
+            Connection connection = DriverManager.getConnection(conn, "root", "");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Products");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.println(metaData.getColumnName(i) + " ");
+            }
+
+            System.out.println();
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    System.out.println(resultSet.getObject(i));
+                }
+                System.out.println();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
